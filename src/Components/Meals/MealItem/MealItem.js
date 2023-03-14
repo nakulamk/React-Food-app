2d64bacf35d3fedd14f28f813e6ccff9a7cf98db
Nakula Gowda <nakulamk@gmail.com>
2023-03-14 10:22:04 +0530

    cart-functionality

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,5 +1,5 @@
-import MealItemForm from "./MealItemForm";
-import classes from "./MealItem.module.css";
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
